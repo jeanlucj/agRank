33cb13219ec8daedaaa4e580b1a17f5ff638eaac
Shuxiao Chen <shuxiaoc@gmail.com>
2016-08-15 09:21:25 -0400

    change AP --> AD

--- R/resDesign.R
+++ R/resDesign.R
@@ -3,7 +3,7 @@
 #' In this simulation,
 #' we only include LM and BT with genotypic information.
 #' We run the procedure 100 times with h^2 = 0.2, 0.5, 0.8 with m = 240 and n = 180, 240, 300, 360.
-#' 17 Experimental designs are included, namely AP, RD, GD1~GD9, KM1~KM6. Please see the vignette for detailed
+#' 17 Experimental designs are included, namely AD, RD, GD1~GD9, KM1~KM6. Please see the vignette for detailed
 #' description of each experimental design.
 #'
 #' @docType data
