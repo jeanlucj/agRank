8da8584aeba1479d9f1f4adca129f11fa3551f5d
Linda-Zhou <lz434@cornell.edu>
2017-06-19 20:39:37 -0400

    Update sgdBT.R

--- R/sgdBT.R
+++ R/sgdBT.R
@@ -11,7 +11,7 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
   #helper function
   #return the value of the function to be minimized
   #as well as the gradient w.r.t. index-th observation
-  targetBT = function(index, score, data, mu, sigma){
+  targetBT = function(index, score, adherence, data, mu, sigma){
 
     #let m be the number of varieties,
     #let n be the number of farmers.
@@ -27,11 +27,11 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
 
     #the first nvar element is the gradient for score
     #the last nobs element is the gradient for adherence
-    gradient = rep(0, nvar)
+    gradient = rep(0, nvar + nobs)
 
     #initialize
     inv_sigma = solve(sigma)
-    target_value = as.numeric( 0.2*(t(score - mu) %*% inv_sigma %*% (score - mu)))
+    target_value = as.numeric(0.5 * (t(score - mu) %*% inv_sigma %*% (score - mu)))
     gradient[1:nvar] = 1 / nobs * inv_sigma %*% (score - mu)
 
     #loop over all observations
@@ -53,15 +53,15 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
           win = ranking[j]
           lose = ranking[k]
 
-          exp_term = exp( (score[win] - score[lose]))
+          exp_term = exp(-adherence[i] * (score[win] - score[lose]))
 
           #update the value of the target function
           target_value = target_value + log(1 + exp_term)
 
           if(index == i){
             #update the gradient w.r.t. score
-            gradient[win] = gradient[win] - exp_term / (1 + exp_term)
-            gradient[lose] = gradient[lose] +  exp_term / (1 + exp_term)
+            gradient[win] = gradient[win] - adherence[i] * exp_term / (1 + exp_term)
+            gradient[lose] = gradient[lose] + adherence[i] * exp_term / (1 + exp_term)
 
             #update the gradient w.r.t. adherence
             gradient[nvar + i] = gradient[nvar + i] +
@@ -93,7 +93,7 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
   #the first nvar element is the score
   #the last nobs element is the adherence
   param = start
-  target = rnorm(niter,10,1)
+  target = rep(0, niter)
 
   flag = TRUE
   #loop until the convergence criteria are met
@@ -102,12 +102,12 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
     for(i in 1:nobs){
       niter = niter + 1
       score_temp = param[1:nvar]
-      
+      adherence_temp = param[(nvar + 1):(nvar + nobs)]
       #evaluate the log-posterior as well as the gradient
       #only used for small dataset (where we want to decide the learning rate)
       #if used for big dataset, where we don't want to
       #evaluate log-posterior everytime, the function should be modified
-      res_temp = targetBT(i, score_temp,  data, mu, sigma)
+      res_temp = targetBT(i, score_temp, adherence_temp, data, mu, sigma)
 
       #store the value of the target function
       target[niter] = res_temp[[1]]
@@ -136,4 +136,6 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
 
   }
 
-  return(list(value = target, niter = niter, score = param[1:nvar])
+  return(list(value = target, niter = niter, score = param[1:nvar],
+              adherence = param[(nvar + 1):(nvar + nobs)]))
+}
