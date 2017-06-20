e7d836b986a1fbc612aa2cd340b87be90b67b2ac
Linda-Zhou <lz434@cornell.edu>
2017-06-19 22:14:00 -0400

    Update sgdBT.R

--- R/sgdBT.R
+++ R/sgdBT.R
@@ -129,7 +129,7 @@ sgdBT = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
 
       #update the parameters
       gradientList <- append(gradientList, gradient)
-      gradientSum <- sqrt(as.vector(gradientList) %*% as.vecotr(t(gradientList)))
+      gradientSum <- sqrt(as.vector(gradientList) %*% as.vector(t(gradientList)))
       updateRule[1,i] <- (0.1 / gradientSum) * gradient
       temporaryparam[1,i] = param[1,i] - updateRule[1,i]
         }
