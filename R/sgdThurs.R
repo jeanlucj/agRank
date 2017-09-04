0f28767c654780c08d09f075a54c6e3d2965692d
Linda-Zhou <lz434@cornell.edu>
2017-09-04 17:14:16 -0400

    Update sgdThurs.R

--- R/sgdThurs.R
+++ R/sgdThurs.R
@@ -1,162 +1,101 @@
 #' @export
-nobs = nrow(data)
-nvar = ncol(data)
-sgdThurs = function(data, mu, sigma, rate=0.001, maxiter = 1000, tol = 1e-9, start, decay = 1.1){
+sgdThurs <- function(data, mu, sigma, rate=0.1, maxiter=1000, tol=1e-9, start, decay=1.1){
   #let m be the number of varieties,
   #let n be the number of farmers.
   #data is an n*m matrix,
   #data(i, j) represents the rank of variety i by farmer j
   #the entry where varieties are not included is 0
-  
+
   #rate is the step size/learning rate
-  
+
   #helper function
   #return the value of the function to be minimized
-  #as well as the gradient w.r.t. index-th observation
-  targetThurs = function(index, score, data, mu, sigma){
+  #as well as the gradient w.r.t. whichObs-th observation
+  targetThurs <- function(whichObs, scores, data, mu, inv_sigma){
     #let m be the number of varieties,
     #let n be the number of farmers.
     #data is an n*m matrix,
     #data(i, j) represents the rank of variety i by farmer j
     #the entry where varieties are not included is 0
-    
+
     #(mu, sigma) are parameters of the normal prior
-    
-    nobs = nrow(data)
-    nvar = ncol(data)
-    colnames(data) = 1:nvar #assign labels to varieties
-    
-    #the first nvar element is the gradient for score
-    #the last nobs element is the gradient for adherence
-    gradient = rep(0, nvar)
+
+    #nobs <- nrow(data)
+    #nvar <- ncol(data)
+    colnames(data) <- 1:nvar #assign labels to varieties
     #initialize
-    inv_sigma = solve(sigma)
-    target_value = as.numeric(0.5 * (t(score - mu) %*% inv_sigma %*% (score - mu)))
-    gradient[1:nvar] = 1 / nobs * inv_sigma %*% (score - mu)
-    
+    target_value <- as.numeric(0.5 * (t(scores - mu) %*% inv_sigma %*% (scores - mu)))
+    gradient <- 1 / nobs * inv_sigma %*% (scores - mu)
+
     #loop over all observations
     for(i in 1:nobs){
-      
+
       #calculate the ranking of the form A>B>C...
-      ranks = data[i, ][data[i, ] != 0]
-      ranking = as.numeric(names(sort(ranks)))
-      
+      ranks <- data[i, ][data[i, ] !=0]
+      ranking <- as.numeric(names(sort(ranks)))
+
       #the length of i-th observation
-      nrank = length(ranks)
-      
+      nrank <- length(ranks)
+
       #loop over all pairwise comparisons
       for(j in 1:(nrank - 1)){
-        
         for(k in (j + 1):nrank){
-          
           #ranking[j] wins over ranking[k]
-          win = ranking[j]
-          lose = ranking[k]
-          
+          win <- ranking[j]
+          lose <- ranking[k]
+
           #calculate the term involved with cdf of std. normal
-          quant = (score[win] - score[lose]) / sqrt(2)
-          cdf_term = pnorm(quant)
-          
+          quant <- (scores[win] - scores[lose]) / sqrt(2)
+          cdf_term <- pnorm(quant)
+
           #update the value of the target function
-          target_value = target_value - log(cdf_term)
-          
-          if(i == index){
-            #update the gradient w.r.t. score
-          grad_change = (1 / cdf_term) * (1 / sqrt(2 * pi)) *
-          exp(-0.5 * quant^2) * (1/ sqrt(2))
-          gradient[win] = gradient[win] - grad_change
-          gradient[lose] = gradient[lose] + grad_change
-            
-            #update the gradient w.r.t. adherence
-            
-          }
-          
-          
+          target_value <- target_value - log(cdf_term)
+
+          grad_change <- (1 / cdf_term) * (1 / sqrt(2 * pi)) *
+            exp(-0.5 * quant^2) * (1/ sqrt(2))
+          gradient[win] <- gradient[win] - grad_change / nobs
+          gradient[lose] <- gradient[lose] + grad_change / nobs
         }
       }
-      
     }
-    
-    return(list(value = target_value, gradient = gradient))
-    
-  }
-  
-  
+    return(list(value=target_value, gradient=gradient))
+  }#END targetThurs
 
-  
+
   #initialize
-  #the first nvar element is the score
-  #the last nobs element is the adherence
-  nobs = nrow(data)
-  nvar = ncol(data)
-  colnames(data) = 1:nVarieties #assign labels to varieties
-  inv_sigma = solve(sigma)
-  start = rnorm(ncol(data),10,1)
+  nobs <- nrow(data)
+  nvar <- ncol(data)
+  colnames(data) <- 1:nVarieties #assign labels to varieties
+  start <- scores <- rnorm(nVarieties)
+  inv_sigma <- solve(sigma)
+
   #initialize
-  niter = 0
-  #the first nvar element is the score
-  #the last nobs element is the adherence
-  data = matrix(unlist(data), ncol=ncol(data), byrow=FALSE)
-  data <- cbind(1, data)
-  #parse data into input and output
-  inputData = data[,1:ncol(data)-1]
-  outputData = data[,ncol(data)]
-  param = start
-  target = rep(0, niter)
-  gradient= list()
- 
-  temporaryparam = matrix(0,ncol=length(param), nrow=1)
-  updateRule = matrix(0, ncol=length(param), nrow=1)
-  gradientList = matrix(NA,nrow=1, ncol=0)
-  stochasticList = sample(1:nrow(data), 1000, replace=TRUE)
-  flag = TRUE
+  niter <- 0
+  nTargetWorse <- 0
+  targets <- NULL
+  gradients <- NULL
+  flag <- TRUE
   #loop until the convergence criteria are met
   while(flag){
-    
-    for(i in 1:nobs){
-      niter = niter + 1
-      #error = (param%*%inputData[stochasticList[niter],]) - outputData[stochasticList[niter]]
-      score_temp = param[1:nvar]
-      
-      #evaluate the log-posterior as well as the gradient
-      #only used for small dataset (where we want to decide the learning rate)
-      #if used for big dataset, where we don't want to
-      #evaluate log-posterior everytime, the function should be modified
-      res_temp = targetThurs(i,score_temp, data, mu, sigma)
-      #store the value of the target function
-      target[niter] = res_temp$value
-      
-      #extract the gradient
-      gradient[[niter]] = res_temp[[2]]
-      #update the parameters
-      gradientList = append(gradientList,gradient[[niter]])
-      gradientSum = as.numeric(sqrt(t(gradientList) %*% (gradientList)))
-      updateRule = as.numeric(rate/ gradientSum) * gradient[[niter]]
-      temporaryparam = param - rate*updateRule
-      
-      
-      #check the convergence criteria: square of the change of target values
-      if(niter > 1){
-        if((target[niter] - target[niter - 1]) ^ 2 < tol | niter > maxiter){
-          flag = FALSE
-          break
-        }
-        
-        #update learning rate if the target value don't decrease
-        if((target[niter - 1] - target[niter]) / target[niter - 1] < 0){
-          rate = rate / decay
-          
-        }
-        
+    niter <- niter + 1
+    res_temp <- targetThurs(1, scores, data, mu, inv_sigma)
+    targets <- c(targets, res_temp$value)
+    scores <- scores - rate * res_temp$gradient
+    gradients <- rbind(gradients, res_temp$gradient)
+
+    #check the convergence criteria: square of the change of target values
+    if(niter > 1){
+      if((targets[niter] - targets[niter - 1]) > 0){
+        nTargetWorse <- nTargetWorse + 1
+        rate <- rate / decay
       }
-      param=temporaryparam
-      
-    }
-    
-  }
-  
-  return(list(value = target, niter = niter, score = param[1:nvar],gradient))
-  
-}
- 
+      if((targets[niter] - targets[niter - 1]) ^ 2 < tol | niter > maxiter){
+        flag <- FALSE
+      }
+    }#END niter > 1
+  }#END while flag
+
+  return(list(value=targets[niter], niter=niter, scores=scores, startScores=start, targets=targets, gradients=gradients, nTargetWorse=nTargetWorse))
+}#END sgdThurs
+
 
