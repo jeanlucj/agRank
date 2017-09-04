38829dd415858518f1711d00aaeb90a40cf58849
Linda-Zhou <lz434@cornell.edu>
2017-09-04 17:16:36 -0400

    Update sgdPL.R

--- R/sgdPL.R
+++ R/sgdPL.R
@@ -1,6 +1,5 @@
 #' @export
-sgdPL = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay = 1.1){
-
+sgdPL <- function(data, mu, sigma, rate=0.1, maxiter=1000, tol=1e-9, start, decay=1.1){
   #let m be the number of varieties,
   #let n be the number of farmers.
   #data is an n*m matrix,
@@ -9,11 +8,10 @@ sgdPL = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
 
   #rate is the step size/learning rate
 
-
   #helper function
   #return the value of the function to be minimized
-  #as well as the gradient w.r.t. index-th observation
-  targetPL = function(index, score, adherence, data, mu, sigma){
+  #as well as the gradient w.r.t. whichObs-th observation
+  targetPL <- function(whichObs, scores, data, mu, inv_sigma){
     #let m be the number of varieties,
     #let n be the number of farmers.
     #data is an n*m matrix,
@@ -22,17 +20,13 @@ sgdPL = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
 
     #(mu, sigma) are parameters of the normal prior
 
-    nobs = nrow(data)
-    nvar = ncol(data)
-    colnames(data) = 1:nvar #assign labels to varieties
-
-    #the first nvar element is the gradient for score
-    #the last nobs element is the gradient for adherence
-    gradient = rep(0, nvar + nobs)
+    nobs <- nrow(data)
+    nvar <- ncol(data)
+    colnames(data) <- 1:nvar #assign labels to varieties
+    inv_sigma <- solve(sigma)
     #initialize
-    inv_sigma = solve(sigma)
-    target_value = as.numeric(0.5 * (t(score - mu) %*% inv_sigma %*% (score - mu)))
-    gradient[1:nvar] = 1 / nobs * inv_sigma %*% (score - mu)
+    target_value <- as.numeric(0.5 * (t(scores - mu) %*% inv_sigma %*% (scores - mu)))
+    gradient <- 1 / nobs * inv_sigma %*% (scores - mu)
 
     #loop over all observations
     for(i in 1:nobs){
@@ -44,109 +38,65 @@ sgdPL = function(data, mu, sigma, rate, maxiter = 1000, tol = 1e-9, start, decay
       #the length of i-th observation
       nrank = length(ranks)
 
-
       #loop over all pairwise comparisons
       for(j in 1:(nrank - 1)){
-
         sum_temp = 0
         sum_temp2 = 0
-
         for(k in (j + 1):nrank){
-
           #ranking[j] wins over ranking[k]
           win = ranking[j]
           lose = ranking[k]
 
-          sum_temp = sum_temp + exp(-adherence[i] * (score[win] - score[lose]))
-          sum_temp2 = sum_temp2 + exp(-adherence[i] * (score[win] - score[lose])) *
-            (-score[win] + score[lose])
+          sum_temp = sum_temp + exp(-(scores[win] - scores[lose]))
+          sum_temp2 = sum_temp2 + exp(-(scores[win] - scores[lose])) *
+            (-scores[win] + scores[lose])
 
-        }
 
-        #update the value of the target function
-        target_value = target_value + log(1 + sum_temp)
+          #update the value of the target function
+          target_value = target_value + log(1 + sum_temp)
 
-        if(index == i){
-          #update the gradient w.r.t. score
           after_j = ranking[(j + 1):nrank] #varieties ranked after variety j
-          gradient[after_j] = gradient[after_j] + (adherence[i] / (1 + sum_temp)) *
-            exp(-adherence[i] * (score[win] - score[after_j]))
-          gradient[win] = gradient[win] - (adherence[i] / (1 + sum_temp)) * sum_temp
+          gradient[after_j] = gradient[after_j] + (1 / (1 + sum_temp)) *
+            exp(-(scores[win] - scores[after_j]))/nobs
+          gradient[win] = gradient[win] - (1/ (1 + sum_temp)) * sum_temp/nobs
 
-          #update the gradient w.r.t. adherence
-          gradient[nvar + i] = gradient[nvar + i] + (1 / (1 + sum_temp)) * sum_temp2
 
         }
-
       }
-
-
     }
+    return(list(value=target_value, gradient=gradient))
+  }#END targetPL
 
-    return(list(value = target_value, gradient = gradient))
-
-
-  }
-
-
-
-
-
-
-  nobs = nrow(data)
-  nvar = ncol(data)
-  colnames(data) = 1:nvar #assign labels to varieties
-  inv_sigma = solve(sigma)
 
   #initialize
-  niter = 0
-  #the first nvar element is the score
-  #the last nobs element is the adherence
-  param = start
-  target = rep(0, niter)
+  start <- scores <- rnorm(nVarieties)
 
-  flag = TRUE
+
+  #initialize
+  niter <- 0
+  nTargetWorse <- 0
+  targets <- NULL
+  gradients <- NULL
+  flag <- TRUE
   #loop until the convergence criteria are met
   while(flag){
+    niter <- niter + 1
+    res_temp <- targetPL(1, scores, data, mu, inv_sigma)
+    targets <- c(targets, res_temp$value)
+    scores <- scores - rate * res_temp$gradient
+    gradients <- rbind(gradients, res_temp$gradient)
 
-    for(i in 1:nobs){
-      niter = niter + 1
-      score_temp = param[1:nvar]
-      adherence_temp = param[(nvar + 1):(nvar + nobs)]
-      #evaluate the log-posterior as well as the gradient
-      #only used for small dataset (where we want to decide the learning rate)
-      #if used for big dataset, where we don't want to
-      #evaluate log-posterior everytime, the function should be modified
-      res_temp = targetPL(i, score_temp, adherence_temp, data, mu, sigma)
-
-      #store the value of the target function
-      target[niter] = res_temp[[1]]
-
-      #extract the gradient
-      gradient = res_temp[[2]]
-
-      #update the parameters
-      param = param - rate * gradient
-
-      #check the convergence criteria: square of the change of target values
-      if(niter > 1){
-        if((target[niter] - target[niter - 1]) ^ 2 < tol | niter > maxiter){
-          flag = FALSE
-          break
-        }
-
-        #update learning rate if the target value don't decrease
-        if((target[niter - 1] - target[niter]) / target[niter - 1] < 0){
-          rate = rate / decay
-        }
+    #check the convergence criteria: square of the change of target values
+    if(niter > 1){
+      if((targets[niter] - targets[niter - 1]) > 0){
+        nTargetWorse <- nTargetWorse + 1
+        rate <- rate / decay
       }
+      if((targets[niter] - targets[niter - 1]) ^ 2 < tol | niter > maxiter){
+        flag <- FALSE
+      }
+    }#END niter > 1
+  }#END while flag
 
-
-    }
-
-  }
-
-  return(list(value = target, niter = niter, score = param[1:nvar],
-              adherence = param[(nvar + 1):(nvar + nobs)]))
-
-}
+  return(list(value=targets[niter], niter=niter, scores=scores, startScores=start, targets=targets, gradients=gradients, nTargetWorse=nTargetWorse))
+}#END sgdPL
